7fa330062cf6d60e8faf42d79652e2235f87dfc3
ash-21 <ashraful.nitol@gmail.com>
2017-10-07 14:43:14 +0600

    password hashing

---
+++ action_page_password_hash.php
@@ -0,0 +1,40 @@
+<html>
+<body>
+
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = 'asdfzxcv';
+$db = 'aaaDatabase';
+
+$conn =new mysqli($dbhost,$dbuser,$dbpass,$db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$userName = addslashes($_POST['userName']);
+$profession = addslashes($_POST['profession']);
+$homeAddress = addslashes($_POST['homeAddress']);
+$workAddress = addslashes($_POST['workAddress']);
+$email = addslashes($_POST['email']);
+$phone = addslashes($_POST['phoneNo']);
+$password = $_POST['password'];
+$password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+$query = "insert into users ".
+"(name,profession,homeAddress,workAddress,email,phone,password_hash) ".
+"values( '$userName' , '$profession' , '$homeAddress' , '$workAddress' , '$email' , '$phone' , '$password_hash' )";
+
+if ($conn->query($query) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
+
+</body>
+</html>
+
