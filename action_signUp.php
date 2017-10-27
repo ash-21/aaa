465cbf75a90f146917253cdcc98ea89c93ae6574
Md Sakib Anwar <sakibanwar1149095@gmail.com>
2017-10-27 12:34:27 +0600

    strategy pattern applied in signup

--- action_signUp.php
+++ action_signUp.php
@@ -1,35 +1,6 @@
 <?php
-//Mail requirements
-/********************************/
-/********************************/
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
-require 'Exception.php';
-require 'PHPMailer.php';
-require 'SMTP.php';
-
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;
-$mail->Username = "automatedappointmentassistance@gmail.com";
-$mail->Password = "nituashraf";
-$mail->setFrom('automatedappointmentassistance@gmail.com', 'AAA');
-$mail->addReplyTo('automatedappointmentassistance@gmail.com', 'AAA');
-/*******************************/
-/********************************/
-function __autoload($class_name) {
-  require_once $class_name . '.php';
-}
-
-$database_object = singleton_database::getInstance();
-
-$conn = $database_object->getDatabase();
+require 'user_client_profile.php';
+require 'strategy_signup.php';
 
 $userName = addslashes($_POST['userName']);
 $profession = addslashes($_POST['profession']);
@@ -38,45 +9,20 @@ $workAddress = addslashes($_POST['workAddress']);
 $email = addslashes($_POST['email']);
 $phone = addslashes($_POST['phoneNo']);
 $password = $_POST['password'];
-$password_hash = password_hash($password, PASSWORD_BCRYPT);
 
-$query = "insert into users ".
-"(name,profession,homeAddress,workAddress,email,phone,password) ".
-"values( '$userName' , '$profession' , '$homeAddress' , '$workAddress' , '$email' , '$phone' , '$password_hash' )";
+$userObject = new user($userName,$profession,$workAddress,$homeAddress,$email,$phone,$password);
 
-$to      = $email;
-$subject = 'AAA account is ready';
-$message = 'Thank you for joining with us.';
-
-if ($conn->query($query) === TRUE) {
-    echo "<h>New record created successfully</h>"."<br>";
-    /********************************/
-    /********************************/
-    $mail->addAddress($email, $userName);
-	$mail->Subject = $subject;
-	$mail->Body = $message;
-	if (!$mail->send()) {
-    	echo "Mailer Error: " . $mail->ErrorInfo;
-	} else {
-    	echo "Message sent!";
-	}
-	function save_mail($mail)
-	{
-    	$path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-    	$imapStream = imap_open($path, $mail->Username, $mail->Password);
-		$result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    	imap_close($imapStream);
-    	return $result;
-	}
-	/********************************/
-	/********************************/
-} else {
-    echo "Error: " . $query . "<br>" . $conn->error;
-}
-
-print "<script>
+$signupObject = new strategy_signup('user');
+if($signupObject->signUp($userObject)===TRUE){
+	print "<script>
 		window.location='index.html';
 		</script>";
+}
+else {
+	print "<script>
+		window.location='errorSignup.html';
+		</script>";
+}
 
 $conn->close();
 
